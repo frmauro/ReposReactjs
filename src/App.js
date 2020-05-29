204853772bc86d354b9c1fb2bd2a494d1064d8b6
francisco <frmauro8@hotmail.com>
2020-05-29 18:24:08 -0300

    create main styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Routes from './routes';
+import GlobalStyle from './styles/global';
 
 //https://api.github.com
 
 function App() {
   return (
+    <>
+    <GlobalStyle/>
      <Routes/>
+     </>
   );
 }
 
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import {FaGithub, FaPlus} from 'react-icons/fa';
+
+import { Container, Form, SubmitButton } from './styles';
 
 export default function Main(){
     return(
-        <h1>Main</h1>
+        <Container>
+            <h1>
+                <FaGithub size={25}/>
+                Meus Repositorios
+            </h1>
+
+            <Form onSubmit={()=>{}}>
+                <input type="text" placeholder="Adicionar repositorios" />
+            </Form>
+
+            <SubmitButton>
+                <FaPlus color="#FFF" size={14}/>
+            </SubmitButton>
+
+        </Container>
     )
 }
