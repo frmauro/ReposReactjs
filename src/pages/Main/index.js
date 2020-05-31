2192ee07b5e2ef1bcb61ee8328f293e240eeb691
francisco <frmauro8@hotmail.com>
2020-05-31 09:19:51 -0300

    create api.js

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, {useState, useCallback} from 'react';
 import {FaGithub, FaPlus} from 'react-icons/fa';
 
 import { Container, Form, SubmitButton } from './styles';
 
+import api from '../../services/api';
+
 export default function Main(){
+
+    const [newRepo, setNewRepo] = useState('');
+    const [repositorios, setRepositorios] = useState([]);
+
+    const handleSubmit = useCallback((e)=>{
+        e.preventDefault();
+
+            async function submit(){
+                 const response =  await api.get(`repos/${newRepo}`);
+                 const data = {
+                     name: response.data.full_name,
+                  } 
+
+                 setRepositorios([...repositorios, data]);
+                 setNewRepo('');
+             }
+
+             submit();
+    }, [newRepo, repositorios]);
+
+    function handleInputChange(e){
+        setNewRepo(e.target.value);
+    }
+
     return(
         <Container>
             <h1>
@@ -11,13 +37,21 @@ export default function Main(){
                 Meus Repositorios
             </h1>
 
-            <Form onSubmit={()=>{}}>
-                <input type="text" placeholder="Adicionar repositorios" />
-            </Form>
+            <Form onSubmit={handleSubmit}>
+                <input 
+                    type="text" 
+                    placeholder="Adicionar repositorios"
+                    value={newRepo}
+                    onChange={handleInputChange}
+                 />
 
             <SubmitButton>
                 <FaPlus color="#FFF" size={14}/>
             </SubmitButton>
+
+
+            </Form>
+
 
         </Container>
     )
